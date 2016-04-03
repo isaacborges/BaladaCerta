3d5704a42ff4cc7ea9988455a95333877461f562
geisonszo <geisonszo@gmail.com>
2016-04-03 12:54:58 -0300

    Criacao da classe Promotion.java

---
+++ PartiuBalada/app/src/main/java/unlv/erc/partiubalada/model/Promotion.java
@@ -0,0 +1,13 @@
+package unlv.erc.partiubalada.model;
+
+public class Promotion {
+
+    int promotionCode;
+    String type;
+
+    void promotion(){}
+    int getPromotionCode(){}
+    void setPromotionCode(){}
+    String getType(){}
+    setType(){}
+}
