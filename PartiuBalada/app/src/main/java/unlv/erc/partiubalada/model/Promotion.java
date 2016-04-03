9513495976c3924224444f8b2b16274c66c06c2a
Dandara Aranha <dandaraaranha@gmail.com>
2016-04-03 13:23:35 -0300

    Adding in Promotion

--- PartiuBalada/app/src/main/java/unlv/erc/partiubalada/model/Promotion.java
+++ PartiuBalada/app/src/main/java/unlv/erc/partiubalada/model/Promotion.java
@@ -5,9 +5,24 @@ public class Promotion {
     private int promotionCode;
     private String type;
 
-    void promotion(){}
-    int getPromotionCode(){}
-    void setPromotionCode(){}
-    String getType(){}
-    setType(){}
+    public Promotion(int promotionCode, String type) {
+        this.promotionCode = promotionCode;
+        this.type = type;
+    }
+
+    public int getPromotionCode() {
+        return promotionCode;
+    }
+
+    public void setPromotionCode(int promotionCode) {
+        this.promotionCode = promotionCode;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
