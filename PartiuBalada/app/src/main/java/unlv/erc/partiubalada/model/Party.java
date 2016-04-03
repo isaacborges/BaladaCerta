2adc0f45f2040512ed55ad3179c6a94adc5b4e80
geisonszo <geisonszo@gmail.com>
2016-04-03 13:54:27 -0300

    adding variables and methods in Party.java

--- PartiuBalada/app/src/main/java/unlv/erc/partiubalada/model/Party.java
+++ PartiuBalada/app/src/main/java/unlv/erc/partiubalada/model/Party.java
@@ -1,6 +1,56 @@
 package unlv.erc.partiubalada.model;
 
+import java.lang.String;
+
 public class Party {
 
+    private String partyName;
+    private int idParty;
+    private float latitude;
+    private float longitude;
+    private String type;
+    private float price;
+    private String startTime;
+    private String endTime;
+    private float amountOfStars;
 
+   public void party() {
+
+    }
+
+    public float getAmountOfStars() {
+        return amountOfStars;
+    }
+
+    public String getPartyName() {
+        return partyName;
+    }
+
+    public int getIdParty() {
+        return idParty;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
 }
