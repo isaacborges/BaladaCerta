961fe400e94a922a87ffef89e316cf5c413f1064
Marcos Dourado <marcosdourado92@gmail.com>
2016-04-29 15:04:30 -0300

    Inserting card animation

--- PartiuBalada/app/src/main/java/unlv/erc/partiubalada/view/MainActivity.java
+++ PartiuBalada/app/src/main/java/unlv/erc/partiubalada/view/MainActivity.java
@@ -4,9 +4,14 @@ import android.app.Activity;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
@@ -19,29 +24,31 @@ import java.util.List;
 
 import unlv.erc.partiubalada.R;
 import unlv.erc.partiubalada.model.Party;
+import unlv.erc.partiubalada.model.User;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<Party> parties = new ArrayList<Party>();
+    private ArrayList<Party> parties = new ArrayList<Party>();
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private static String LOG_TAG = "CardViewActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getDataFromFB();
-
-        createList();
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MyRecyclerViewAdapter(getDataFromFB(), MainActivity.this);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
-
-    private void createList() {
-        ArrayAdapter<Party> adapter = new myListAdapter();
-        ListView list = (ListView) findViewById(R.id.partyListView);
-        list.setAdapter(adapter);
-    }
-
-    public void getDataFromFB() {
+    public ArrayList<Party> getDataFromFB() {
         float latitudeTest = (float) 123.5;
         float longitudeTest = 456;
         float priceTest = 50;
@@ -56,51 +63,29 @@ public class MainActivity extends AppCompatActivity {
 
         parties.add(new Party("Zeze di Camargo e Luciano", "zezedicamargo", idTest, latitudeTest,
                 longitudeTest, "Sertanejo", priceTest, startTimeTest, endTimeTest, "Espaço Villa " +
-                "Mix(Agora)",(float) 4));
+                "Mix(Agora)", (float) 4));
 
-        parties.add(new Party("Bloco do Primeiro Beijo", "ensaioprimeirobj", idTest, latitudeTest,
+
+        Party p1 = new Party("Bloco do Primeiro Beijo", "ensaioprimeirobj", idTest, latitudeTest,
                 longitudeTest, "Sertanejo", priceTest, startTimeTest, endTimeTest, "Clube do " +
-                "Congresso(Agora)",(float) 3));
+                "Congresso(Agora)", (float) 3);
 
+        parties.add(p1);
+
+        return parties;
     }
 
-    private class myListAdapter extends ArrayAdapter<Party> {
-        public myListAdapter() {
-            super(MainActivity.this, R.layout.partylistlayout, parties);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View itemView = convertView;
-
-            Typeface openSans;
-
-            if (itemView == null) {
-                itemView = getLayoutInflater().inflate(R.layout.partylistlayout, parent, false);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ((MyRecyclerViewAdapter) mAdapter).setOnItemClickListener(new MyRecyclerViewAdapter
+                .MyClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                Log.i(LOG_TAG, " Clicked on Item " + position);
             }
-
-            Party currentParty = parties.get(position);
-
-            openSans = Typeface.createFromAsset(getAssets(),"OpenSans-CondLight.ttf");
-
-            TextView partyName = (TextView) itemView.findViewById(R.id.partyName);
-            partyName.setText(currentParty.getPartyName());
-            partyName.setTypeface(openSans);
-
-            TextView partyLocality = (TextView) itemView.findViewById(R.id.partyLocation);
-            partyLocality.setText(currentParty.getLocality());
-            partyLocality.setTypeface(openSans);
-
-            String background = currentParty.getPartyImage();
-            int drawableID = getResources().getIdentifier(background, "drawable", getPackageName());
-            ImageView partyImage = (ImageView) itemView.findViewById(R.id.partyImage);
-            partyImage.setBackgroundResource(drawableID);
-
-            RatingBar ratingBar = (RatingBar) itemView.findViewById(R.id.partyRating);
-            ratingBar.setRating(currentParty.getAmountOfStars());
-
-            return itemView;
-        }
-
+        });
     }
+
+
 }
---
+++ PartiuBalada/app/src/main/java/unlv/erc/partiubalada/view/MyRecyclerViewAdapter.java
@@ -0,0 +1,130 @@
+package unlv.erc.partiubalada.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import unlv.erc.partiubalada.R;
+import unlv.erc.partiubalada.model.Party;
+
+public class MyRecyclerViewAdapter extends RecyclerView
+        .Adapter<MyRecyclerViewAdapter
+        .DataObjectHolder> {
+    private static String LOG_TAG = "MyRecyclerViewAdapter";
+    private ArrayList<Party> mDataset;
+    private static MyClickListener myClickListener;
+    private Context context;
+    private int lastPosition = -1;
+    private View view;
+
+    public static class DataObjectHolder extends RecyclerView.ViewHolder
+            implements View
+            .OnClickListener {
+        TextView partyName;
+        TextView partyLocation;
+        ImageView partyImage;
+        RatingBar partyRating;
+        LinearLayout cardRoot;
+
+        public DataObjectHolder(View itemView) {
+            super(itemView);
+            partyName = (TextView) itemView.findViewById(R.id.partyName);
+            partyLocation = (TextView) itemView.findViewById(R.id.partyLocation);
+            partyRating = (RatingBar) itemView.findViewById(R.id.partyRating);
+            partyImage = (ImageView) itemView.findViewById(R.id.partyImage);
+            cardRoot = (LinearLayout) itemView.findViewById(R.id.card_root);
+
+            Log.i(LOG_TAG, "Adding Listener");
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            myClickListener.onItemClick(getAdapterPosition(), v);
+        }
+    }
+
+    public void setOnItemClickListener(MyClickListener myClickListener) {
+        this.myClickListener = myClickListener;
+    }
+
+    public MyRecyclerViewAdapter(ArrayList<Party> myDataset, Context context) {
+        mDataset = myDataset;
+        this.context = context;
+    }
+
+    @Override
+    public DataObjectHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_view_row, parent, false);
+
+        DataObjectHolder dataObjectHolder = new DataObjectHolder(view);
+
+        return dataObjectHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(DataObjectHolder holder, int position) {
+        Typeface openSans = Typeface.createFromAsset(context.getAssets(),
+                "OpenSans-CondLight" +
+                        ".ttf");
+        holder.partyName.setText(mDataset.get(position).getPartyName());
+        holder.partyName.setTypeface(openSans);
+
+        holder.partyLocation.setText(mDataset.get(position).getLocality());
+        holder.partyLocation.setTypeface(openSans);
+
+        holder.partyRating.setRating(mDataset.get(position).getAmountOfStars());
+
+        String background = mDataset.get(position).getPartyImage();
+        int imageID = holder.itemView.getResources().getIdentifier(background, "drawable", "unlv.erc.partiubalada");
+        holder.partyImage.setImageResource(imageID);
+
+        setAnimation(view, position);
+    }
+
+
+    public void addItem(Party dataObj, int index) {
+        mDataset.add(index, dataObj);
+        notifyItemInserted(index);
+    }
+
+    public void deleteItem(int index) {
+        mDataset.remove(index);
+        notifyItemRemoved(index);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public interface MyClickListener {
+        public void onItemClick(int position, View v);
+    }
+
+    private void setAnimation(View viewToAnimate, int position)
+    {
+        // If the bound view wasn't previously displayed on screen, it's animated
+        if (position > lastPosition) {
+            Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
+            viewToAnimate.startAnimation(animation);
+            lastPosition = position;
+        }
+    }
+}
