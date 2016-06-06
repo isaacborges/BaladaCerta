1751f9e8af6a21776068c0446dc83a507cd8817e
Marcos Dourado <marcosdourado92@gmail.com>
2016-06-06 00:09:32 -0300

    setting action to account button on the mainactivity

--- PartiuBalada/app/src/main/java/unlv/erc/partiubalada/view/MainActivity.java
+++ PartiuBalada/app/src/main/java/unlv/erc/partiubalada/view/MainActivity.java
@@ -1,10 +1,17 @@
 package unlv.erc.partiubalada.view;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -45,5 +52,10 @@ public class MainActivity extends AppCompatActivity {
         parties = partyController.getPatiesArray();
 
         partiesReference.addValueEventListener(event);
+    }
+
+    public void onAccountClicked(View view) {
+        Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+        startActivity(intent);
     }
 }
