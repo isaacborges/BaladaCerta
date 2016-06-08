74067890f70312163ad5904e14697414c607d4cf
Marcos Dourado <marcosdourado92@gmail.com>
2016-06-08 18:06:37 -0300

    uploading image on firebase DONE MUTHAFUCKA!

--- PartiuBalada/app/src/main/java/unlv/erc/partiubalada/view/PartyCreateAcitivity.java
+++ PartiuBalada/app/src/main/java/unlv/erc/partiubalada/view/PartyCreateAcitivity.java
@@ -6,7 +6,6 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Environment;
 import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -15,7 +14,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -26,21 +24,17 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 import java.io.FileDescriptor;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 import unlv.erc.partiubalada.R;
-import unlv.erc.partiubalada.model.Party;
 
 public class PartyCreateAcitivity extends AppCompatActivity {
     public static final String CHOOSE_FROM_GALLERY = "Escolher banner";
     public static final String CANCEL = "Cancelar";
+    private static int RESULT_LOAD_IMAGE = 1;
     private FirebaseStorage storage;
     private StorageReference storageRef;
-    private static int RESULT_LOAD_IMAGE = 1;
-
+    private ImageView partyBanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +43,77 @@ public class PartyCreateAcitivity extends AppCompatActivity {
 
         storage = FirebaseStorage.getInstance();
         storageRef = storage.getReferenceFromUrl("gs://project-8420821685282639830.appspot.com");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            String picturePath = chooseImageFromGallery(data);
+
+            uploadImageOnFirebase(picturePath);
+        } else {
+            Log.e("ERROR", "It wasn't possible to get the image");
+        }
+    }
+
+    private void uploadImageOnFirebase(String picturePath) {
+        Uri file = Uri.fromFile(new File(picturePath));
+        StorageReference partyImagesRef = storageRef.child("images/"+file.getLastPathSegment());
+        UploadTask uploadTask = partyImagesRef.putFile(file);
+
+        // Register observers to listen for when the download is done or if it fails
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+                Log.e("Upload", "it was not possible to upload the image");
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                Log.i("Upload","Success");
+            }
+        });
+    }
+
+    private String chooseImageFromGallery(Intent data) {
+        Uri selectedImage = data.getData();
+        String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+        Cursor cursor = getContentResolver().query(selectedImage,
+                filePathColumn, null, null, null);
+        cursor.moveToFirst();
+
+        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+        String picturePath = cursor.getString(columnIndex);
+
+        cursor.close();
+
+        Bitmap bmp = null;
+        try {
+            bmp = getBitmapFromUri(selectedImage);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        partyBanner = (ImageView) findViewById(R.id.partyBanner);
+        partyBanner.setVisibility(ImageView.VISIBLE);
+        partyBanner.setImageBitmap(bmp);
+        return picturePath;
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
     }
 
     public void onChooseBannerClicked(View view) {
@@ -70,48 +135,5 @@ public class PartyCreateAcitivity extends AppCompatActivity {
             }
         });
         builder.show();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-
-            Cursor cursor = getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-            cursor.moveToFirst();
-
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
-
-            ImageView imageView = (ImageView) findViewById(R.id.partyBanner);
-
-            Bitmap bmp = null;
-            try {
-                bmp = getBitmapFromUri(selectedImage);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-
-            imageView.setVisibility(ImageView.VISIBLE);
-            imageView.setImageBitmap(bmp);
-
-        } else {
-            Log.e("ERROR", "It wasn't possible to get the image");
-        }
-    }
-
-    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
-        ParcelFileDescriptor parcelFileDescriptor =
-                getContentResolver().openFileDescriptor(uri, "r");
-        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
-        parcelFileDescriptor.close();
-        return image;
     }
 }
